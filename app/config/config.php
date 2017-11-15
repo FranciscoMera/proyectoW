4953c08ded89ca08d0e5ebed9ff03559038a5c36
FranciscoMera <fcomera.ipn@gmail.com>
2017-11-15 07:56:50 -0600

    Se recuepra la versión del servidor y acceso a la base de datos

--- app/config/config.php
+++ app/config/config.php
@@ -8,7 +8,7 @@
     define("DB_HOST",'localhost');
     define("DB_NAME",'anuarioDB');
     define("DB_USERNAME",'root');
-    define("DB_PASSWORD",'');
+    define("DB_PASSWORD",'root');
 
 
     $con = mysqli_connect(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
